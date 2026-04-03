153f399b0faf175b5ed01295b320e916287e2c61
uliakovalevskaa <yu.kovalevska@ukma.edu.ua>
2026-04-03 14:47:35 +0300

    setup: added CI/CD pipeline

--- scraper.py
+++ scraper.py
@@ -15,40 +15,4 @@ try:
     print(" [+] Секрет успішно отримано з Vault!")
 except Exception as e:
     print(f" [-] Помилка читання з Vault: {e}")
-    INFLUX_TOKEN = "error"
-
-INFLUX_URL = "http://influxdb:8086"
-INFLUX_ORG = "my-org"
-INFLUX_BUCKET = "crypto_data"
-
-db_client = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=INFLUX_ORG)
-write_api = db_client.write_api(write_options=SYNCHRONOUS)
-
-
-def get_crypto_price(coin="bitcoin"):
-    url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin}&vs_currencies=usd"
-    try:
-        response = requests.get(url)
-        data = response.json()
-        if coin in data:
-            return data[coin]['usd']
-        return None
-    except Exception as e:
-        print(f"Помилка під час запиту: {e}")
-        return None
-
-
-if __name__ == "__main__":
-    print("Скрейпер запущено. Збираємо ціну...")
-    while True:
-        price = get_crypto_price()
-        if price:
-            print(f"Поточна ціна BTC: ${price}")
-            point = Point("crypto_price").tag("coin", "BTC").field("price", float(price))
-            try:
-                write_api.write(bucket=INFLUX_BUCKET, org=INFLUX_ORG, record=point)
-                print(" [+] Успішно збережено в базу!")
-            except Exception as e:
-                print(f" [-] Помилка запису в InfluxDB: {e}")
-
-        time.sleep(60)
+    INF
